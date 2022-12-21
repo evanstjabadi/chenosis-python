f7c5820efdd78570d2b266bc1f1d7ff07b413486
evanstjabadi <evansltjabadi@gmail.com>
2022-12-21 22:20:22 +0200

    test get access token + linting

--- chenosis/exceptions.py
+++ chenosis/exceptions.py
@@ -2,10 +2,13 @@ class ChenosisException(Exception):
     """
     Base class for Chenosis Library
     """
+
     pass
+
 
 class InvalidCredentials(ChenosisException):
     """
     Raised after failing to authenticate with Chenosis API
     """
+
     pass
