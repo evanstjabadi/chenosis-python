9c5aaa032747ba25dfd8f2821a60b5ba1c5665f6
evanstjabadi <evansltjabadi@gmail.com>
2022-12-23 18:40:41 +0200

    add negative test

--- tests/test_client.py
+++ tests/test_client.py
@@ -5,7 +5,7 @@ import pytest
 from pytest_httpx import HTTPXMock
 
 from chenosis.client import ChenosisClient
-from chenosis.exceptions import InvalidCredentials
+from chenosis.exceptions import ChenosisAPIError, InvalidCredentials
 
 host = "http://testing.chenosis.io"
 client_id = "TEST_CLIENT_ID"
@@ -69,6 +69,24 @@ def test_get_network_information(httpx_mock: HTTPXMock) -> None:
         response = chenosis_client.get_network_information(phone_number=phone_number)
 
         assert response == {"data": {"country": "SA"}}  # Edit
+
+
+def test_get_network_information_raise_error(httpx_mock: HTTPXMock) -> None:
+    httpx_mock.add_response(
+        status_code=HTTPStatus.SERVICE_UNAVAILABLE,
+        url=f"{host}/mobile/subscriber/{phone_number}/home-location",
+        json={"error": "Service Unavailable"},
+    )
+
+    with mock.patch("chenosis.client.ChenosisClient.authenticate") as mock_authenticate:
+        mock_authenticate.return_value = {"access_token": "short-lived-token"}
+
+        chenosis_client = ChenosisClient(
+            host=host, client_id=client_id, client_secret=client_secret
+        )
+
+        with pytest.raises(ChenosisAPIError):
+            _ = chenosis_client.get_network_information(phone_number=phone_number)
 
 
 def test_get_mobile_carrier_details(httpx_mock: HTTPXMock) -> None:
